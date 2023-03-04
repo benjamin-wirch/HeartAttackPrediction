684134982b2a80ba8002748b31332591c778a641
M K <mkpro118@gmail.com>
2023-03-04 17:14:56 -0600

    updated settings

--- heart_attack_predictor/predictor/models.py
+++ heart_attack_predictor/predictor/models.py
@@ -7,3 +7,5 @@ class User(models.Model):
     sex = models.BooleanField(null=False)
     chest_pain = models.PositiveSmallIntegerField(null=False)
     resting_blood_pressure = models.PositiveIntegerField(null=False)
+    serum_cholestrol = models.PositiveIntegerField(null=False)
+    fasting_blood_sugar = models.PositiveIntegerField(null=False)
