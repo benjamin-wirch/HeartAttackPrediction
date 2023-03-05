2fd2741531923db6b22a86c09f3eed6044594cdd
M K <mkpro118@gmail.com>
2023-03-04 22:30:22 -0600

    tensorflow

--- heart_attack_predictor/main.py
+++ heart_attack_predictor/main.py
@@ -1,49 +1,39 @@
-from neural_network.layers import Dense
-from neural_network.models import Sequential
-from neural_network.model_selection import KFold, train_test_split
-from neural_network.metrics import confusion_matrix, accuracy_score, accuracy_by_label
-from neural_network.preprocess import OneHotEncoder
-import numpy as np
 from data import load_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import numpy as np
+
 
 X, y = load_data()
-print(X.shape)
-print(y.shape)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+print(f'{len(X_train) = }')
+print(f'{len(X_test) = }')
+print(f'{len(y_train) = }')
+print(f'{len(y_test) = }')
 
-labels = np.unique(y)
-encoder = OneHotEncoder().fit(labels)
-y_train = encoder.transform(y_train)
-y_test = encoder.transform(y_test)
-
-net = Sequential()
-
-net.add(
-    Dense(64, input_shape=11, activation='tanh'),
-    Dense(64, activation='tanh'),
-    Dense(2, activation='sigmoid')
+model = Sequential(
+    [
+        Dense(32, input_dim=11, activation='tanh'),
+        Dense(64, activation='tanh'),
+        Dense(16, activation='tanh'),
+        Dense(1, activation='sigmoid'),
+    ]
 )
 
-net.compile(cost='mse', metrics=[
-            'accuracy_score'])
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=100)
 
-kf = KFold(n_splits=5)
+score = model.evaluate(X_test, y_test, verbose=0)
+print("\nTest loss:", format(score[0], ".4f"))
+print("Test accuracy:", score[1])
 
-for train, validate in kf.split(X_train):
-    net.fit(
-        X_train[train],
-        y_train[train],
-        epochs=10,
-        validation_data=(X_train[validate], y_train[validate])
-    )
+# # Cell 8
+# y_softmax = model.predict(X_test)
 
+# y_pc = np.argmax(y_softmax, axis=-1)
 
-predictions = net.predict(X_test, classify=True)
-print()
-print(confusion_matrix(y_test, predictions))
-print()
-print(accuracy_score(y_test, predictions))
-print()
-print(accuracy_by_label(y_test, predictions))
+# y_pred = to_categorical(y_pc)
