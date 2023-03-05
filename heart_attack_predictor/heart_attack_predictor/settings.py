7c2ca17ec1554aacd50d163d71fc54bdb758c0a2
M K <mkpro118@gmail.com>
2023-03-05 03:59:50 -0600

    create js file

--- heart_attack_predictor/heart_attack_predictor/settings.py
+++ heart_attack_predictor/heart_attack_predictor/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-24f*ys^qn+3v_h1qlvpi-zf(w(681r4&i2*rz%gk+fz%p+fvo@
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    # '*',
+    # 'localhost',
+    '*',
 ]
 
 
