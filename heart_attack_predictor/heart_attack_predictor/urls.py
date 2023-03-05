e2ae406e1a41726e4486873c26bbbfba5f3793e4
M K <mkpro118@gmail.com>
2023-03-04 19:14:20 -0600

    base html

--- heart_attack_predictor/heart_attack_predictor/urls.py
+++ heart_attack_predictor/heart_attack_predictor/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from predictor.views import landing, dashboard, signin, signup
+from predictor.views import landing, dashboard, signin, signup, stats
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing),
-    path('dashboard/', dashboard),
-    path('signin/', signin),
-    path('signup/', signup),
+    path('', landing, name='landing'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('signin/', signin, name='signin'),
+    path('signup/', signup, name='signup'),
+    path('stats/', stats, name='stats'),
 ]
--- heart_attack_predictor/predictor/views.py
+++ heart_attack_predictor/predictor/views.py
@@ -1,26 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotAllowed
 from django.contrib.auth.decorators import login_required
 from os import path
+from functools import wraps
 
 TEMPLATE_ROOT = 'predictor'
 
 
+def control_method(permitted: list):
+    def decorator(func):
+        @wraps(func)
+        def view(request):
+            if request.method not in permitted:
+                return HttpResponseNotAllowed(permitted)
+            return func(request)
+
+        return view
+
+    return decorator
+
+
+def redirect_to_dashboard(func):
+    @wraps(func)
+    def view(request):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+
+        func(request)
+
+    return view
+
+
+@control_method(('GET',))
 def landing(request):
     return render(request, path.join(TEMPLATE_ROOT, 'landing.html'))
 
 
+@redirect_to_dashboard
+@control_method(('GET', 'POST',))
 def signin(request):
-    return render(request, path.join(TEMPLATE_ROOT, 'signin.html'))
+    if request.method == 'GET':
+        return render(request, path.join(TEMPLATE_ROOT, 'signin.html'))
 
 
+@redirect_to_dashboard
+@control_method(('GET', 'POST',))
 def signup(request):
-    return render(request, path.join(TEMPLATE_ROOT, 'signup.html'))
+    if request.method == 'GET':
+        return render(request, path.join(TEMPLATE_ROOT, 'signup.html'))
 
 
+@control_method(('GET', 'POST',))
 def dashboard(request):
-    return render(request, path.join(TEMPLATE_ROOT, 'dashboard.html'))
+    if request.method == 'GET':
+        return render(request, path.join(TEMPLATE_ROOT, 'dashboard.html'))
 
 
-@login_required
+@login_required(login_url='signin')
 def stats(request):
-    return render(request, path.join(TEMPLATE_ROOT, 'dashboard.html'))
+    if request.method == 'GET':
+        return render(request, path.join(TEMPLATE_ROOT, 'stats.html'))
